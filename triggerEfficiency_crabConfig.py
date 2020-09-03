4d37f48de79eb5d5b3e245066c1e461675dc567f
Tanmay Mudholkar <tkmudholkar@gmail.com>
2020-09-03 01:03:22 -0500

    Storage moved to T3_US_CMU.

--- triggerEfficiency_crabConfig.py
+++ triggerEfficiency_crabConfig.py
@@ -11,5 +11,5 @@ config.JobType.psetName = "triggerEfficiency_cfg.py"
 config.Data.inputDataset = "/GluGluHToGG_M125_13TeV_amcatnloFXFX_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"
 config.Data.splitting = "FileBased"
 config.Data.unitsPerJob = 1
-config.Data.outLFNDirBase = "/store/group/lpcsusystealth/triggerEfficiencyNTuples"
-config.Site.storageSite = "T3_US_FNALLPC"
+config.Data.outLFNDirBase = "/store/user/tmudholk/triggerEfficiencyNTuples"
+config.Site.storageSite = "T3_US_CMU"
